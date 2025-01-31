9f4a734f3eb18a92c432af56e5bcbcce8c1d5548
sgotenks <pellerei.luca@gmail.com>
2025-01-31 18:28:06 +0100

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -315,6 +315,7 @@ function setup() {
   window.hlx.RUM_MASK_URL = 'full';
   window.hlx.codeBasePath = '';
   window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
+  window.hlx.plugins = new PluginsRegistry();
 
   const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
   if (scriptEl) {
